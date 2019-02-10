0c6774742bdbe6af2b26aea985be302e96cc2a67
Jhorkman Bernal <jhorber95@gmail.com>
2019-02-10 11:51:28 -0500

    create migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_162630) do
+ActiveRecord::Schema.define(version: 2019_02_10_163833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_02_10_162630) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.boolean "rotting", default: false
   end
 
 end
